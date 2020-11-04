2d911a0cd61d46efe9c30cc5f62dd9acf4402d51
yyxxyyzero@163.com <yyxxyyzero@163.com>
2020-11-04 20:52:59 +0800

    init

---
+++ custom.el
@@ -0,0 +1,46 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#1D1E19" "#E74C3C" "#A6E22E" "#E6DB74" "#268bd2" "#F92660" "#66D9EF" "#F8F8F2"])
+ '(custom-safe-themes
+   (quote
+    ("e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "d74c5485d42ca4b7f3092e50db687600d0e16006d8fa335c69cf4f379dbd0eee" "8e959d5a6771b4d1e2177263e1c1e62c62c0f848b265e9db46f18754ea1c1998" "c83c095dd01cde64b631fb0fe5980587deec3834dc55144a6e78ff91ebc80b19" default)))
+ '(fci-rule-color "#555556")
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#FD971F"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#A6E22E"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#525254"))
+ '(objed-cursor-color "#E74C3C")
+ '(pdf-view-midnight-colors (cons "#F8F8F2" "#272822"))
+ '(rustic-ansi-faces
+   ["#272822" "#E74C3C" "#A6E22E" "#E6DB74" "#268bd2" "#F92660" "#66D9EF" "#F8F8F2"])
+ '(vc-annotate-background "#272822")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#A6E22E")
+    (cons 40 "#bbdf45")
+    (cons 60 "#d0dd5c")
+    (cons 80 "#E6DB74")
+    (cons 100 "#edc457")
+    (cons 120 "#f5ad3b")
+    (cons 140 "#FD971F")
+    (cons 160 "#fb7134")
+    (cons 180 "#fa4b4a")
+    (cons 200 "#F92660")
+    (cons 220 "#f33254")
+    (cons 240 "#ed3f48")
+    (cons 260 "#E74C3C")
+    (cons 280 "#c14d41")
+    (cons 300 "#9c4f48")
+    (cons 320 "#77504e")
+    (cons 340 "#555556")
+    (cons 360 "#555556")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
